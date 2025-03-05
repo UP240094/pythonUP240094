bddcbfa6b50dc2ac908a58910412464e7c0bb04b
Omar Mendoza <up240094@alumnos.upa.edu.mx>
2025-03-05 09:22:35 -0600

    .

---
+++ EjerciciosDia2.py
@@ -0,0 +1,29 @@
+first_name = 'Omar'
+last_name = 'Mendoza'
+full_name = 'Omar Mendoza'
+country_name = 'Mexico'
+city_name = 'Aguascalientes' 
+age = '19'
+year = '2025'
+is_married = 'no'
+is_true = 'yes'
+is_light_on = 'yes'
+first_name = 'Omar'; last_name ='Mendoza'; full_name = 'Omar Mendoza'
+print(first_name)
+print(type(first_name))
+print(last_name)
+print(type(last_name))
+print(full_name)
+print(type(full_name))
+print(country_name)
+print(type(country_name))
+print(city_name)
+print(type(city_name))
+print(age)
+print(type(age))
+print(is_married)
+print(type(is_married))
+
+len(first_name)
+num_one = '5'
+num_two = '4'
---
+++ EjerciciosDia3.py
@@ -0,0 +1,9 @@
+
+#Write a script that prompts the user to enter base and height of 
+#the triangle and calculate an area of this triangle (area = 0.5 x b x h
+
+#Ejercicio 1
+# Declara tu edad como una variable entera
+edad = 19
+print("tu edad es:", edad)
+
