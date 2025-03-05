b1631781c5222e5961e11171ee1799a579889346
Omar Mendoza <up240094@alumnos.upa.edu.mx>
2025-03-05 09:40:06 -0600

    ..

--- EjerciciosDia3.py
+++ EjerciciosDia3.py
@@ -5,5 +5,22 @@
 #Ejercicio 1
 # Declara tu edad como una variable entera
 edad = 19
-print("tu edad es:", edad)
+print("tu edad es: ", edad)
 
+#Ejercicio 2
+# Declara tu nombre como una variable float
+altura = 1.70  
+print("tu altura es: ", altura)
+
+#Ejercicio 3
+# Declara una variable con numero complejo
+numero = 3j
+print("tu numero complejo es: ", numero)
+
+#Ejercicio 4
+# Declara un programa que pida al usuario poner la base y la altura de un triangulo y calcule el area del triangulo
+base = float(input("Ingrese la base del triangulo: "))
+altura = float(input("Ingrese la altura del triangulo: "))
+area = 0.5 * base * altura
+print("El area del triangulo es: ", area)
+# Este ejercicio da la base de un triangulo y la altura y calcula el area del triangulo
