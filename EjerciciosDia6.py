f9f69454dbdd9fd7efb1a296ecccb4a13d5d804a
Omar Mendoza <up240094@alumnos.upa.edu.mx>
2025-03-11 08:18:19 -0600

    .

---
+++ EjerciciosDia6.py
@@ -0,0 +1,28 @@
+#Excercises: Level 1
+#Create an empty tuple
+empty_tuple = ()
+
+#Create a tuple containing names of your 
+# sisters and your brothers 
+
+Familiy = ('Diego', 'Mauricio', 'Emilio', 'Fernanda')
+
+#Join brothers and sisters tuples and assign it to siblings
+siblings = Familiy
+
+#How many siblings do you have?
+print(len(siblings))
+
+#Modify the siblings tuple and add the name of 
+# your father and mother and assign it to family_members
+family_members = ('Diego', 'Mauricio', 'Emilio', 'Fernanda', 'Omar', 'Lorena', 'Yveth')
+
+#Exercises: Level 2
+#Unpack siblings and parents from family_members
+print("Siblings"[0:3])
+print("Parents"[5:7])
+
+#Create fruits, vegetables and animal products tuples. 
+# Join the three tuples 
+# and assign it to a variable called food_stuff_tp.
+
