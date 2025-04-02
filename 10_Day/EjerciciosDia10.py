9a7ede4bcd67c85d3a2e601849a3356eccffce1a
Omar Mendoza <up240094@alumnos.upa.edu.mx>
2025-04-02 09:21:25 -0600

    .

---
+++ 10_Day/EjerciciosDia10.py
@@ -0,0 +1,16 @@
+##1. Iterate 0 to 10 using for loop, do the same using while loop.
+
+print('Ejercicio 1 nivel 1: Itera del 0 al 10 usando el bucle for, haz lo mismo usando el bucle while.')
+
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for number in numbers:
+    print(number)
+
+##2. Iterate 10 to 0 using for loop, do the same using while loop.
+
+print('Ejercicio 2 nivel 1: Itera del 0 al 10 usando el bucle for, haz lo mismo usando el bucle while.')
+
+number = 10
+while number > -1:
+    print(number)
+    number = number -1 
